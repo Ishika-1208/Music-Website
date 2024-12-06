822a922eda171e2ccad1c370ac5fd7bf8be39160
Ishika-1208 <guptaishika1208@gmail.com>
2024-12-06 10:27:28 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import './App.css';
-import Signup from './Components/Signup'
-import Login from './Components/Login'
-import ForgotPs2 from './Components/ForgotPs2'
-import NewPs from './Components/NewPs'
+import Signup from './Components/Signup';
+import Login from './Components/Login';
+import ForgotPs2 from './Components/ForgotPs2';
+import NewPs from './Components/NewPs';
+import Homepage from './Components/Homepage';
 import {
-   BrowserRouter as Router,
-     Route,
-     Routes,
-  } from "react-router-dom";
-
-// import Homepage from './Components/Homepage'
- import React from "react";
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import React from "react";
 
 function App() {
   return (
-    <Router>
-    <Routes>
-          <Route path="" element={<NewPs/>}/>
-          <Route path="/signup"  element={<Signup/>}/>
-          <Route path="/login"  element={<Login />}/>
-          <Route path="/ForgotPs2" element={<ForgotPs2/>}/>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/new-password" element={<NewPs />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/forgot-password" element={<ForgotPs2 />} />
         </Routes>
-    </Router>
+      </Router>
+    </>
   );
 }
 
